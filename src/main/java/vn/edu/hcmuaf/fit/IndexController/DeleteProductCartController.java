a7cc6552abe6a36357c70ff39d0a0f5d7c5deb3e
NguyenHuyHiep-20130258 <20130258@st.hcmuaf.edu.vn>
2022-12-12 17:50:36 +0700

    sua so luong san pham, xoa san pham trong cart

---
+++ src/main/java/vn/edu/hcmuaf/fit/IndexController/DeleteProductCartController.java
@@ -0,0 +1,29 @@
+package vn.edu.hcmuaf.fit.IndexController;
+
+import vn.edu.hcmuaf.fit.beans.cart.Cart;
+import vn.edu.hcmuaf.fit.beans.cart.CartKey;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "DeleteProductCartController", value = "/DeleteProductCartController")
+public class DeleteProductCartController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String id = request.getParameter("id");
+        String color = request.getParameter("color");
+        String size = request.getParameter("size");
+        Cart cart = (Cart) request.getSession().getAttribute("cart");
+        CartKey key = new CartKey(id, color, size);
+        cart.getData().remove(key);
+        request.getSession().setAttribute("cart", cart);
+        request.getRequestDispatcher("ajax/ajax_Cart.jsp").forward(request, response);
+    }
+}
---
+++ src/main/java/vn/edu/hcmuaf/fit/IndexController/IncreaseDecreaseQuantityController.java
@@ -0,0 +1,35 @@
+package vn.edu.hcmuaf.fit.IndexController;
+
+import vn.edu.hcmuaf.fit.beans.cart.Cart;
+import vn.edu.hcmuaf.fit.beans.cart.CartKey;
+import vn.edu.hcmuaf.fit.beans.product.Product;
+import vn.edu.hcmuaf.fit.services.ProductService;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "IncreaseDecreaseQuantityController", value = "/IncreaseDecreaseQuantityController")
+public class IncreaseDecreaseQuantityController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String idUpd = request.getParameter("idUpd");
+        int amount = Integer.parseInt(request.getParameter("amount"));
+        String color = request.getParameter("color");
+        String size = request.getParameter("size");
+        Cart cart = (Cart) request.getSession().getAttribute("cart");
+        CartKey key = new CartKey(idUpd, color, size);
+        if (cart.getData().containsKey(key)) {
+            Product product = cart.getData().get(key);
+            product.setQuantity_cart(amount);
+        }
+        request.getSession().setAttribute("cart", cart);
+        request.getRequestDispatcher("ajax/ajax_Cart.jsp").forward(request, response);
+    }
+}
