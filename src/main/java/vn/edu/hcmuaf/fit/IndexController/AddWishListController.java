48bce9201b661ed85c9f2cc11aa05bd76238e3a1
NguyenHuyHiep-20130258 <20130258@st.hcmuaf.edu.vn>
2022-12-14 18:45:54 +0700

    Hoan thien them wishlist

---
+++ src/main/java/vn/edu/hcmuaf/fit/IndexController/AddWishListController.java
@@ -0,0 +1,28 @@
+package vn.edu.hcmuaf.fit.IndexController;
+
+import vn.edu.hcmuaf.fit.beans.product.Product;
+import vn.edu.hcmuaf.fit.beans.wishlist.WishList;
+import vn.edu.hcmuaf.fit.services.ProductService;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "AddWishListController", value = "/AddWishListController")
+public class AddWishListController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String id = request.getParameter("id").substring(11);
+        WishList wishList = (WishList) request.getSession().getAttribute("wishList");
+        Product product = ProductService.getInstance().getProductAndDetails(id);
+        wishList.put(product);
+        request.getSession().setAttribute("wishlist", wishList);
+        request.getRequestDispatcher("ajax/ajax_addWishListSuccessful.jsp").forward(request, response);
+    }
+}
